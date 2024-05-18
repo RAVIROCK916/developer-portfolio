0645e7fbb926761b9d4f1091a6e77a5283aae3f9
RAVIROCK916 <prt15@iitbbs.ac.in>
2024-05-18 17:42:24 +0530

    ui: add hero section

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,15 @@
+import Hero from "@/components/Hero";
+import { FloatingNav } from "@/components/ui/FloatingNav";
+import { FaHome } from "react-icons/fa";
+
 export default function Home() {
   return (
-    <main className="relative mx-auto flex min-h-screen flex-col items-center justify-center overflow-hidden bg-black-100 px-5 py-2 sm:px-10">
+    <main className="relative mx-auto flex min-h-screen flex-col items-center justify-center overflow-hidden bg-black-100 px-5 sm:px-10">
       <div className="max-x-7xl w-full">
-        <h1>Hello there</h1>
+        <FloatingNav navItems={[
+          {name: "Home", link: "/", icon: <FaHome />},
+        ]} />
+        <Hero />
       </div>
     </main>
   );
