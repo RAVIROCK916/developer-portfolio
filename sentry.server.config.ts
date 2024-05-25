fcf4510cdb6b8c8b67c2558d895799090f9ecc59
RAVIROCK916 <prt15@iitbbs.ac.in>
2024-05-25 12:12:27 +0530

    feat: sentry setup added sentry to the application which is basically to get any errors and feedback from the customers. It helps to manage and maintain the app.

---
+++ sentry.server.config.ts
@@ -0,0 +1,19 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://7082136872931c2701f8122a9e477720@o4507310556839936.ingest.us.sentry.io/4507310558412800",
+
+  // Adjust this value in production, or use tracesSampler for greater control
+  tracesSampleRate: 1,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+
+  // Uncomment the line below to enable Spotlight (https://spotlightjs.com)
+  // spotlight: process.env.NODE_ENV === 'development',
+  
+});
